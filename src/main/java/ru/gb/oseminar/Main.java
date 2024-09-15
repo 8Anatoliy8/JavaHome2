0fed3201bee7189c77b8325d4dff02aa9b2a6355
Anatoliy Bogdebich <tolya.bogdevich@mail.ru>
2024-09-15 22:58:27 +0300

    Initial commit

---
+++ src/main/java/ru/gb/oseminar/Main.java
@@ -0,0 +1,14 @@
+package ru.gb.oseminar;
+
+public class Main {
+    public static void main(String[] args) {
+        Market market = new Market();
+        Human human1 = new Human("Ivan");
+        Human human2 = new Human("Vladimir");
+        Human human3 = new Human("Anatoliy");
+        market.acceptToMarket(human1);
+        market.acceptToMarket(human2);
+        market.acceptToMarket(human3);
+        market.update();
+    }
+}
